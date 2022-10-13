7a84d34359fdb0b708b1aeb1d2b024ee550d84ae
AMO15310 <wachiraamos402@gmail.com>
2022-10-14 01:21:00 +0300

    Upload home

---
+++ home/homeScript.js
@@ -0,0 +1,21 @@
+const homeContainer = document.querySelector(".table2");
+
+const Jsondata = localStorage.getItem("JsonData");
+const cleanData = JSON.parse(Jsondata);
+
+cleanData.forEach((element) => {
+  const tabrow = document.createElement("tr");
+  tabrow.innerHTML = `
+    <td>${element.clientName}</td>
+    <td>${element.phoneno}</td>
+    <td>${element.meterv}</td>
+    <td>${element.initialr}</td>
+    <td>${element.finalr}</td>
+    <td>${element.consumed}</td>
+    <td>${element.unitCost}</td>
+    <td>${element.totalCost}</td>
+    <td>${element.totalpayed}</td>
+    <td>${element.balance}</td>
+    `;
+  homeContainer.appendChild(tabrow);
+});
