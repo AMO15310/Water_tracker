42641a6337be3c5f8cf7f55406b7615af7122e6a
AMO15310 <wachiraamos402@gmail.com>
2022-10-16 14:19:49 +0300

    Upload files

--- globalScript.js
+++ globalScript.js
@@ -3,7 +3,8 @@ const elem = document.querySelector("body");
 
 switchMode.addEventListener("click", () => {
   elem.classList.toggle("dark-mode");
-  let theme = body.classList.value;
+  let theme = elem.classList.value;
+  console.log(theme);
   localStorage.setItem("theme", theme);
 });
 const thim = localStorage.getItem("theme");
