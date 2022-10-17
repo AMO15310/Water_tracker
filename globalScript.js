3c491573431686e13a68d41c898a7f08b987d38a
AMO15310 <wachiraamos402@gmail.com>
2022-10-17 11:52:27 +0300

    Update files

--- globalScript.js
+++ globalScript.js
@@ -4,7 +4,6 @@ const elem = document.querySelector("body");
 switchMode.addEventListener("click", () => {
   elem.classList.toggle("dark-mode");
   let theme = elem.classList.value;
-  console.log(theme);
   localStorage.setItem("theme", theme);
 });
 const thim = localStorage.getItem("theme");
