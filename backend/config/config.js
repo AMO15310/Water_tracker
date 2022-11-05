c0c4668c0f2a7b5c0f19a49f8243b074aa5ae88f
AMO15310 <wachiraamos402@gmail.com>
2022-11-06 02:12:14 +0300

    Upload file

---
+++ backend/config/config.js
@@ -0,0 +1,16 @@
+const mysql2 = require("mysql2");
+// const dotenv = require("dotenv").config();
+const pass = process.env.pass;
+
+const db = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "Water_tracker",
+  password: pass,
+});
+
+db.connect((err) => {
+  if (err) return err;
+  return console.log(`Connected to database.. `);
+});
+module.exports = db;
