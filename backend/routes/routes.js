48a1a3295d98920205edade0cdbc08194cf58559
AMO15310 <wachiraamos402@gmail.com>
2022-11-08 23:30:32 +0300

    changes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getSingle,
-  getUsers,
-  postUser,
-  delUser,
-  updateTable,
-} = require("../controllers/controllers");
-router.get("/users", getUsers);
-router.post("/user", postUser);
-router.get("/user/:id", getSingle);
-router.delete("/user/:id", delUser);
-router.put("/user/:id", updateTable);
+const controllers = require("../controllers/controllers");
+
+router.get("/totalunits", controllers.sumCons);
+router.get("/totalpaid", controllers.sumPaid);
+router.get("/totalbal", controllers.getSumOfBal);
+router.get("/usersbal", controllers.getTopBalance);
+router.get("/users", controllers.getUsers);
+router.post("/user", controllers.postUser);
+router.get("/user/:id", controllers.getSingle);
+router.delete("/user/:id", controllers.delUser);
+router.put("/user/:id", controllers.updateTable);
 module.exports = router;
