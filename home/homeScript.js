e19d950c7a10ece5352ca53f05ec7ffe39180f8b
AMO15310 <wachiraamos402@gmail.com>
2022-11-08 23:37:23 +0300

    changes

--- home/homeScript.js
+++ home/homeScript.js
@@ -1,24 +1,54 @@
 const homeContainer = document.querySelector(".table2");
+const balance = document.querySelector(".bal");
+const consumed = document.querySelector(".cons");
+const AmountPaid = document.querySelector(".paid");
 
-const Jsondata = localStorage.getItem("JsonData");
 const bodyy = document.querySelector("body");
-const cleanData = JSON.parse(Jsondata);
 
-cleanData.forEach((element) => {
-  const tabrow = document.createElement("tr");
-  tabrow.innerHTML = `
-    <td>${element.clientName}</td>
-    <td>${element.phoneno}</td>
-    <td>${element.meterv}</td>
-    <td>${element.initialr}</td>
-    <td>${element.finalr}</td>
-    <td>${element.consumed}</td>
-    <td>${element.unitCost}</td>
-    <td>${element.totalCost}</td>
-    <td>${element.totalpayed}</td>
-    <td>${element.balance}</td>
-    `;
-  homeContainer.appendChild(tabrow);
-});
+fetch(`http://localhost:3320/usersbal`)
+  .then((resp) => resp.json())
+  .then((records) => {
+    records.forEach((record) => {
+      const tr = document.createElement("tr");
+
+      tr.innerHTML = `
+        <td>${record.name}</td>
+        <td>${record.contact}</td>
+        <td>${record.meterNumber}</td>
+        <td>${record.initialUnits}</td>
+        <td>${record.finalUnits}</td>
+        <td>${record.consumedUnits}</td>
+        <td>${record.unitCost}</td>
+        <td>${record.totalCost}</td>
+        <td>${record.paid}</td>
+        <td>${record.balance}</td>
+
+
+        `;
+      homeContainer.appendChild(tr);
+    });
+  });
+
 const theme = localStorage.getItem("theme");
 bodyy.classList = theme;
+let balurl = "http://localhost:3320/totalbal";
+fetch(balurl)
+  .then((res) => res.json())
+  .then((bal) => {
+    balance.innerHTML = bal[0]["SUM(balance)"] + " " + "Kes";
+  });
+
+let uniturl = "http://localhost:3320/totalunits";
+fetch(uniturl)
+  .then((res) => res.json())
+  .then((unit) => {
+    consumed.innerHTML = unit["SUM(consumedUnits)"] + " " + "Units";
+  });
+
+let paidurl = "http://localhost:3320/totalpaid";
+
+fetch(paidurl)
+  .then((res) => res.json())
+  .then((paid) => {
+    AmountPaid.innerHTML = paid[0]["SUM(paid)"] + " " + "Kes";
+  });
