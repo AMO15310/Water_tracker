bbcbb54501a7532c2479e81c8835ef6f4ca2b75d
AMO15310 <wachiraamos402@gmail.com>
2022-11-12 23:50:36 +0300

    Changes

--- settings/settings.js
+++ settings/settings.js
@@ -1,71 +1,72 @@
 const newEmail = document.querySelector(".email-input");
 const newPass = document.querySelector(".new-pass");
-const confPass = document.querySelector(".conf-pass");
+const oldPass = document.querySelector(".conf-pass");
 const saveButton = document.querySelector(".save");
 const cancelButton = document.querySelector(".cancel");
-const pm1 = document.querySelector(".pm1");
-const pm2 = document.querySelector(".pm2");
-const pm3 = document.querySelector(".pm3");
-const pm4 = document.querySelector(".pm4");
-const pm5 = document.querySelector(".pm5");
-const pm6 = document.querySelector(".pm6");
-let password = 1234;
-let username = "benmax@gmail.com";
+const mess = document.querySelector(".mess");
 
+const token = localStorage.getItem("token");
+
+!token ? (location.href = "../auth/login.html") : "";
 cancelButton.addEventListener("click", () => {
   newEmail.value = "";
   newPass.value = "";
   confPass.value = "";
 });
-
 saveButton.addEventListener("click", () => {
-  if (newPass.value !== confPass.value) {
-    // console.log("Passwords must be same!!");
-    pm1.style.display = "block";
+  if (!newEmail.value || !newPass.value || !oldPass.value) {
+    mess.innerText = "Fill all fields";
+    mess.style.display = "flex";
     setTimeout(() => {
-      pm1.style.display = "none";
-    }, 3000);
+      mess.style.display = "none";
+    }, 3500);
+    return false;
+  }
 
-    return false;
-  }
-  if (newPass.value == "" || confPass.value == "") {
-    // console.log("Password cannot be an empty string!!");
-    pm2.style.display = "block";
-    setTimeout(() => {
-      pm2.style.display = "none";
-    }, 3000);
-    return false;
-  }
   if (newPass.value.length < 6) {
-    // console.log("Password Must be atleast 6 characters");
-    pm3.style.display = "block";
+    mess.innerText = "Password must not be less than 6 !";
+    mess.style.display = "flex";
     setTimeout(() => {
-      pm3.style.display = "none";
-    }, 3000);
+      mess.style.display = "none";
+    }, 3500);
     return false;
   }
-  if (newEmail.value !== "") {
-    username = newEmail.value;
-    // console.log(`Username Changed and its ${username}`);
-    pm4.style.display = "block";
-    setTimeout(() => {
-      pm4.style.display = "none";
-    }, 3000);
-  }
+  const newValues = {
+    email: newEmail.value,
+    password: newPass.value,
+    oldpass: oldPass.value,
+  };
+  const url = "http://localhost:3320/changepass";
+  fetch(url, {
+    method: "PATCH",
+    headers: {
+      token: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newValues),
+  })
+    .then((resp) => resp.json())
+    .then((msg) => {
+      if (msg.message == "jwt expired") {
+        location.href = "../auth/login.html";
+      }
 
-  if (newPass.value == confPass.value || newPass.value !== "") {
-    password = newPass.value;
-    // console.log(`New password is set and is ${password}`);
-    pm5.style.display = "block";
-    setTimeout(() => {
-      pm5.style.display = "none";
-    }, 3000);
-    //   return false;
-  } else {
-    // console.log("Try again");
-    pm6.style.display = "block";
-    setTimeout(() => {
-      pm6.style.display = "none";
-    }, 3000);
-  }
+      if (msg.message == "Password changed successfully") {
+        mess.innerText = msg.message;
+        mess.style.display = "flex";
+        mess.style.color = "white";
+        mess.style.backgroundColor = "#109045";
+
+        setTimeout(() => {
+          mess.style.display = "none";
+          mess.style.backgroundColor = "#f4d6d2";
+          mess.style.color = "#70211a";
+        }, 3500);
+      }
+      mess.innerText = msg.message;
+      mess.style.display = "flex";
+      setTimeout(() => {
+        mess.style.display = "none";
+      }, 3500);
+    });
 });
